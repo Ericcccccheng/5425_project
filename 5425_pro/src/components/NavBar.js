d87a30655caffce9995baac55d2917177839fc59
Wenjie Chen <monroechen1@gmail.com>
2024-04-23 23:40:24 +1000

    Upload Image

--- 5425_pro/src/components/NavBar.js
+++ 5425_pro/src/components/NavBar.js
@@ -12,6 +12,8 @@ const NavBar = () => {
     const [isPopupOpen, setPopupOpen] = useState(false);
     const [buttonClicked, setButtonClicked] = useState(false);
     const popupRef = useRef(null);
+    const ImagePopupRef = useRef(null);
+    const [imageData, setImageData] = useState(null);
 
     const openPopup = (event) => {
         event.stopPropagation();
@@ -43,10 +45,32 @@ const NavBar = () => {
         setButtonClicked(false);
     };
 
+    const handleFileUpload = (event) => {
+        const file = event.target.files[0];
+        if (!file) {
+            console.error('No file selected');
+            return;
+        }
+
+        const reader = new FileReader();
+        reader.onloadend = () => {
+            const imageData = reader.result;
+            localStorage.setItem('uploadedImage', imageData);
+            console.log('Image uploaded and stored in localStorage.');
+
+            setImageData(imageData);
+        };
+        reader.readAsDataURL(file);
+    };
+
     const handleClickOutside = (event) => {
         if (popupRef.current && !popupRef.current.contains(event.target)) {
             closeSearch();
+        }
+        if (ImagePopupRef.current && !ImagePopupRef.current.contains(event.target)) {
             closePopup();
+            localStorage.removeItem('uploadedImage');
+            setImageData(null);
         }
     };
 
@@ -56,7 +80,6 @@ const NavBar = () => {
             document.removeEventListener("click", handleClickOutside);
         };
     }, []);
-
 
     return (
         <>
@@ -70,8 +93,8 @@ const NavBar = () => {
                 <div className="fun_pop1">
                     <div className="button-container">
                         <div className="column">
-                            <Link to="/toSpring"> 
-                            <button>Spring</button>
+                            <Link to="/toSpring">
+                                <button>Spring</button>
                             </Link>
                             <button>Button 2</button>
                             <button>Button 3</button>
@@ -96,6 +119,7 @@ const NavBar = () => {
                     </div>
                 </div>
             </div>
+
             <div></div>
             {/* <div className="function3">
                 Function 3
@@ -130,20 +154,46 @@ const NavBar = () => {
 
 
             <div className={`upload_button ${buttonClicked ? "clicked" : ""}`}>
-                <button className="upload" onClick={openPopup}><RiUpload2Fill className="upl"/></button>
+                <button className="upload" onClick={openPopup}><RiUpload2Fill className="upl" /></button>
 
                 {isPopupOpen && (
-                    <div className="popup" ref={popupRef}>
-                        <span className="close" onClick={closePopup}></span>
+                    <div className="popup" ref={ImagePopupRef}>
                         <div className="upload_box">
                             <div className="up_title">Upload</div>
-                            <FaCirclePlus className="up_logo"/>
-                            <button className="up_back">Back</button>
+                            <input
+                                type="file"
+                                id="fileInput"
+                                onChange={handleFileUpload}
+                                style={{ display: 'none' }}
+                            />
+                            <label htmlFor="fileInput">
+                                {imageData ? (
+                                    <>
+                                        <div className="up_image">
+                                            <img src={imageData} alt="Uploaded" className="upp" />
+                                        </div>
+
+                                        <div className="up_container">
+                                            <button className="up_back">Back</button>
+                                            <button className="up_finish">Finish</button>
+                                        </div>
+
+                                    </>
+                                ) : (
+                                    <>
+                                        <FaCirclePlus className="up_logo" />
+                                        <div>
+                                            <button className="up_back_ori">Back</button>
+                                        </div>
+                                    </>
+
+
+                                )}
+                            </label>
                         </div>
                     </div>
                 )}
             </div>
-
         </>
     )
 }
